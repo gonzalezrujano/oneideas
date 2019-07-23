027eb913646aba39f5d41b94a045cd35852c2fdf
Gabriel Ortega(GaboDevelop) <gabriel.enr1998@gmail.com>
2019-07-23 11:34:35 -0400

    iniciando prueba add evento 8

--- resources/js/components/Pages/configuracion/Eventos/Add.js
+++ resources/js/components/Pages/configuracion/Eventos/Add.js
@@ -95,7 +95,7 @@ export default class Add extends React.Component {
 
     handleChange(event) {
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value = target.value;
         if(target.type === 'checkbox'){
             if(target.checked == "m"){
                 console.log("if")
@@ -109,21 +109,25 @@ export default class Add extends React.Component {
         this.setState({
           [name]: value
         })
+
+        console.log(this.state)
     }
 
     handleHoraFecha(event){
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value = target.value;
         const name = target.name;
         this.setState({
           [name]: value
         })
+        console.log(this.state)
     }
 
     handleSubmit(e){
         e.preventDefault();
         let formData = new FormData();
         let s = this.state
+        console.log(s)
 
         let ubicacion = $('input[name=ubicacion]:checked', '#form-add-evento').val();
         console.log(s.ubicacion)
