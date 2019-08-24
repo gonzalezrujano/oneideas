97586ce05bb05585d3ef54a30b04a97b40c0f311
Gabriel Ortega(GaboDevelop) <gabriel.enr1998@gmail.com>
2019-08-23 20:12:28 -0400

    implementacion de librerias necesarias para seats io , implemantada key de prueba

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 require("./paho");
 require("./bootstrap");
+require("babel-polyfill");
 
 /**
  * Next, we will create a fresh React component instance and attach it to
--- resources/js/components/Pages/configuracion/Planos/Planos.js
+++ resources/js/components/Pages/configuracion/Planos/Planos.js
@@ -1,24 +1,19 @@
 import React, { Component } from "react";
+//import { SeatsioSeatingChart } from "@seatsio/seatsio-react";
+import { SeatsioDesigner } from "@seatsio/seatsio-react";
 
 export default class Planos extends Component {
-    constructor () {
-        this.state = {
-            numChildren: 0
-        };
-    }
-
-   
-
     render() {
         return (
             <div>
                 <button type="button">Agregar svg</button>
-
-                <svg>
-                    <circle cx={50} cy={50} r={10} fill="red" />
-                </svg>
+                {/*
+                <SeatsioSeatingChart
+                    publicKey="648ce2f6-f6c0-49e6-9155-7a99440e748a"
+                    event="29ef56b2-30ab-4c3f-8934-e3faf614afac"
+/>*/}
+                <SeatsioDesigner designerKey="90e8b482-18d2-4747-8b3d-f77f9fe4391a" />
             </div>
-            
         );
     }
 }
