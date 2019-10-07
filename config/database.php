caae4f1cba682ca766529093c006015ae0fc2be6
Leonardo Gonzalez <gonzalezrujano@gmail.com>
2019-10-07 05:49:25 -0400

    fix: Ajustada autenticación de Mongo en producción

--- config/database.php
+++ config/database.php
@@ -90,11 +90,11 @@ return [
             'host'     => env('DB_HOST_MONGO', 'localhost'),
             'port'     => env('DB_PORT_MONGO', 27017),
             'database' => env('DB_DATABASE_MONGO', 'forge'),
-            /* 'username' => env('DB_USERNAME_MONGO', ''),
+            'username' => env('DB_USERNAME_MONGO', ''),
             'password' => env('DB_PASSWORD_MONGO', ''),
             'options'  => [
                 'database' => env('DB_DATABASE_AUTHENTICATION_MONGO', 'admin') // sets the authentication database required by mongo 3
-            ] */
+            ]
         ],
 
     ],
