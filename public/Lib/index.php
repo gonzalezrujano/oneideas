5b96b948b8c5ce3254130c32acc5af7cfc39730c
GerardoAGL96 <gerardoagl96@gmail.com>
2019-11-06 15:36:57 -0400

    Social stream configured for auto update

--- public/Lib/index.php
+++ public/Lib/index.php
@@ -55,17 +55,19 @@
     echo social_stream(
         array(
             'id' => '1',
-            'type' => ($_GET['presentacion']) ? $_GET['presentacion'] : 'wall',
+            'type' => isset($_GET['presentacion']) ? $_GET['presentacion'] : 'wall',
             'network' => obtenerRedesAConsultar(),
-            'theme' => ($_GET['tema']) ? $_GET['tema'] : 'sb-modern-light',
+            'theme' => isset($_GET['tema']) ? $_GET['tema'] : 'sb-modern-light',
             'itemwidth' => 250,
-            'results' => ($_GET['cantidadDeResultados']) ? $_GET['cantidadDeResultados'] : 30,
+            'results' => isset($_GET['cantidadDeResultados']) ? $_GET['cantidadDeResultados'] : 30,
             'cache' => 720,
             'debuglog' => 1,
             'iframe' => 'media',
             'breakpoints' => array('4', '4', '3', '3', '2', '1', '1'),
             'filter_search' => false,
             'add_files' => true,
+            'live' => true,
+            'live_interval' => 2,
             'order' => 'random'
         )
     );
